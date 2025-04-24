e1a8f0a33d22853dbf35f4e76ef9a5fb3a69c827
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-04-24 15:26:26 +0900

    refactor : @DirtiesContext 제거

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -3,19 +3,29 @@ package roomescape;
 import static org.hamcrest.core.Is.is;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.update("""
+            SET REFERENTIAL_INTEGRITY FALSE;
+            TRUNCATE TABLE reservation;
+            ALTER TABLE reservation ALTER COLUMN id RESTART WITH 1;
+            TRUNCATE TABLE reservation_time;
+            ALTER TABLE reservation_time ALTER COLUMN id RESTART WITH 1;
+            SET REFERENTIAL_INTEGRITY TRUE;
+            """);
+    }
 
     @Test
     void 일단계() {
@@ -38,37 +48,4 @@ public class MissionStepTest {
                 .statusCode(200)
                 .body("size()", is(0));
     }
-
-//    @Test
-//    void 삼단계() {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("name", "브라운");
-//        params.put("date", "2023-08-05");
-//        params.put("time", "15:40");
-//
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(params)
-//                .when().post("/reservations")
-//                .then().log().all()
-//                .statusCode(201)
-//                .body("id", is(1));
-//
-//        RestAssured.given().log().all()
-//                .when().get("/reservations")
-//                .then().log().all()
-//                .statusCode(200)
-//                .body("size()", is(1));
-//
-//        RestAssured.given().log().all()
-//                .when().delete("/reservations/1")
-//                .then().log().all()
-//                .statusCode(204);
-//
-//        RestAssured.given().log().all()
-//                .when().get("/reservations")
-//                .then().log().all()
-//                .statusCode(200)
-//                .body("size()", is(0));
-//    }
 }
